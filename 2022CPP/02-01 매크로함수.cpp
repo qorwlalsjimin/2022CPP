f37c56cd2fb2ae5a5daf3c80007e93df7a3e4468
지민 백 <qorwlalsjimin@naver.com>
2022-04-01 10:08:13 +0900

    매크로함수 완성

--- 2022CPP/02-01 매크로함수.cpp
+++ 2022CPP/02-01 매크로함수.cpp
@@ -1,8 +1,8 @@
 ﻿#include <iostream>
 using namespace std;
-#define jegob(A) A*A
+#define jegob(A) (A)*(A)
 int main() {
 	cout << jegob(3+1) << endl; //4*4가 아닌 의도치 않은 결과
-	cout << 3+1 * 3+1 << endl; //위의 것과 같음
+	cout << (3+1) * (3+1) << endl; //위의 것과 같음
 	return 0;
 }
