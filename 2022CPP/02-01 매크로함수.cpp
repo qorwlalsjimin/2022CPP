6e0d684c172a21bf1499c6bbb56fa18ae222b94e
지민 백 <qorwlalsjimin@naver.com>
2022-04-01 10:05:31 +0900

    매크로함수 오류사례

--- 2022CPP/02-01 매크로함수.cpp
+++ 2022CPP/02-01 매크로함수.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define jegob(A) A*A
 int main() {
-	cout << jegob(3) << endl;
-	cout << 3 * 3 << endl; //위의 것과 같음
+	cout << jegob(3+1) << endl; //4*4가 아닌 의도치 않은 결과
+	cout << 3+1 * 3+1 << endl; //위의 것과 같음
 	return 0;
 }
