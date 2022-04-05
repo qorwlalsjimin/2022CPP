2c9b9eacab9b52a2deaafa7059e7c22fd85b4c2b
지민 백 <qorwlalsjimin@naver.com>
2022-04-05 11:16:36 +0900

    참조 변수로 바꾸기

--- 2022CPP/02 - 03 참조.cpp
+++ 2022CPP/02 - 03 참조.cpp
@@ -1,16 +1,16 @@
 ﻿#include <iostream>
 using namespace std;
 
-void plus2(int* num)
+void plus2(int& num)
 {
 	//num이 포인터 변수이기에 *로 써서 내구값을 처리
-	*num += 2;
+	num += 2;
 }
 
 int main() {
 	int a = 3;
 	//포인터 매개변수 num에게 주소값으로 전달
-	plus2(&a);
+	plus2(a);
 	cout << a << endl;
 
 	return 0;
