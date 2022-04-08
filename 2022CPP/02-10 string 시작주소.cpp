af4b1eb7995ce87ec0b7612b4580e33d7ad23f52
지민 백 <qorwlalsjimin@naver.com>
2022-04-08 09:54:17 +0900

    string 시작주소

---
+++ 2022CPP/02-10 string 시작주소.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	char cI[10] = "aaa";
+	printf("%d\n", cI);  //aaa
+
+	string cppI = "aaa";
+	printf("%s\n", cppI.c_str());
+	cout << cppI.c_str() << endl; //3
+	return 0;
+}
