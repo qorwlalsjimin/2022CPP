768e0b23272ecc166c2298ac2074239709ca5ae4
지민 백 <qorwlalsjimin@naver.com>
2022-04-08 09:43:59 +0900

    string 길이

---
+++ 2022CPP/02-09 string 길이.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	char cI[10] = "aaa";
+	printf("%d\n", strlen(cI));  //3
+
+	string cppI = "aaa";
+	cout << cppI.size() << endl; //3
+	cout << cppI.length() << endl; //3
+	return 0;
+}
