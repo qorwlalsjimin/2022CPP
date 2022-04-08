2959b9ce65c8e394153ddc2e918778ee6bb3ed88
지민 백 <qorwlalsjimin@naver.com>
2022-04-08 09:03:56 +0900

    string 덧붙이기(append)

---
+++ 2022CPP/02-07 string 덧붙이기(append).cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	char cMunja[20];
+	strcpy(cMunja, "c_insert");
+	strcat(cMunja, "++");
+	printf("%s\n", cMunja);
+
+	// 주의사항 "cpp_insert"+"++" 하면 에러 발생
+	string cppMunja;
+	cppMunja = "cpp_insert";	// cpp_insert
+	cppMunja += "++";			// cpp_insert++
+	cout << cppMunja << endl;
+}
