829bf5f75047554ab3c58d02cf975cb159576da8
지민 백 <qorwlalsjimin@naver.com>
2022-04-19 12:03:53 +0900

    멤버함수 정의를 class 바깥으로

--- 2022CPP/03-01 class 생성자.cpp
+++ 2022CPP/03-01 class 생성자.cpp
@@ -11,17 +11,21 @@ private:
 public:
 	//생성자 : 객체가 생성되면 자동으로 호출되는 함수
 	//반환형을 갖고있지 않는다
-	Student() {
-		nHakbun = 1234;
-		sName = "이사랑";
-		cout << "학번이 등록되었습니다." << endl;
-	}
-
-	void show() {
-		cout << "학번은 " << nHakbun << "입니다" << endl;
-		cout << "이름은 " << sName << "입니다" << endl;
-	}
+	Student();
+	void show();
 };
+
+Student::Student() {
+	nHakbun = 1234;
+	sName = "이사랑";
+	cout << "학번이 등록되었습니다." << endl;
+}
+
+void Student::show() {
+	cout << "학번은 " << nHakbun << "입니다" << endl;
+	cout << "이름은 " << sName << "입니다" << endl;
+}
+
 int main() {
 	//기본생성자
 	Student stu;
