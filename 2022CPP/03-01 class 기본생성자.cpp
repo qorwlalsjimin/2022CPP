2ba31a902157e040fa9cb46c7f694c9fd1d1013e
지민 백 <qorwlalsjimin@naver.com>
2022-04-19 11:55:13 +0900

    생성자

--- 2022CPP/03-01 class 기본생성자.cpp
+++ 2022CPP/03-01 class 기본생성자.cpp
@@ -7,13 +7,18 @@ class Student
 private:
 	int nHakbun;
 	string sName;
-	//아래와 같은 생성자를 만들어줌
-	//Student(){}
+
+public:
+	//생성자 : 객체가 생성되면 자동으로 호출되는 함수
+	//반환형을 갖고있지 않는다
+	Student() {
+		nHakbun = 1234;
+		sName = "이사랑";
+		cout << "학번이 등록되었습니다." << endl;
+	}
 };
 int main() {
 	//기본생성자
-	Student student1;
-	Student student2 = student1;
-
+	Student stu;
 	return 0;
 }
