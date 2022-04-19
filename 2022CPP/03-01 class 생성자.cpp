2c9dcb3b5f8fa9c8bfe9ab93e18a2f0d3b3651af
지민 백 <qorwlalsjimin@naver.com>
2022-04-19 12:12:03 +0900

    생성자 오버로딩

--- 2022CPP/03-01 class 생성자.cpp
+++ 2022CPP/03-01 class 생성자.cpp
@@ -12,12 +12,19 @@ public:
 	//생성자 : 객체가 생성되면 자동으로 호출되는 함수
 	//반환형을 갖고있지 않는다
 	Student();
+	Student(int Hakbun, string Name);
 	void show();
 };
 
 Student::Student() {
 	nHakbun = 1234;
 	sName = "이사랑";
+	cout << "학번이 등록되었습니다." << endl;
+}
+
+Student::Student(int Hakbun, string Name) {
+	nHakbun = Hakbun;
+	sName = Name;
 	cout << "학번이 등록되었습니다." << endl;
 }
 
@@ -28,7 +35,10 @@ void Student::show() {
 
 int main() {
 	//기본생성자
-	Student stu;
-	stu.show();
+	Student stu1 = Student();
+	stu1.show();
+
+	Student stu2 = Student(1111,"BJM");
+	stu2.show();
 	return 0;
 }
