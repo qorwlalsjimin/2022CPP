aa774a38ff0c861e4b6cc49e225e950925ccd402
지민 백 <qorwlalsjimin@naver.com>
2022-05-10 11:54:21 +0900

    this 포인터 추가

--- 2022CPP/03-01 class 생성자.cpp
+++ 2022CPP/03-01 class 생성자.cpp
@@ -12,19 +12,19 @@ public:
 	//생성자 : 객체가 생성되면 자동으로 호출되는 함수
 	//반환형을 갖고있지 않는다
 	Student();
-	Student(int Hakbun, string Name);
+	Student(int nHakbun, string sName);
 	void show();
 };
 
 Student::Student() {
-	nHakbun = 1234;
-	sName = "이사랑";
+	this->nHakbun = 1234;
+	this->sName = "이사랑";
 	cout << "학번이 등록되었습니다." << endl;
 }
 
-Student::Student(int Hakbun, string Name) {
-	nHakbun = Hakbun;
-	sName = Name;
+Student::Student(int nHakbun, string sName) {
+	this->nHakbun = nHakbun;
+	this->sName = sName;
 	cout << "학번이 등록되었습니다." << endl;
 }
 
