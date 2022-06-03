7332162294d2f2d7e07d2edace307721a484b67e
지민 백 <qorwlalsjimin@naver.com>
2022-06-03 10:13:56 +0900

    연산자 오버로딩2

--- 2022CPP/04-01 연산자 오버로딩(기본).cpp
+++ 2022CPP/04-01 연산자 오버로딩(기본).cpp
@@ -35,8 +35,8 @@ Point Point::operator+(const Point& rhs) {
 int main(void) {
 	Point p1(1, 2);
 	Point p2(2, 3);
-	Point sum = p1.operator+(p2); //p1 + p2
+	Point sum = p1 + p2; //p1.operater+(p2)
 
-	sum.ShowXY(); //3,5
+	sum.ShowXY(); // (3, 5) 출력
 	return 0;
 }
