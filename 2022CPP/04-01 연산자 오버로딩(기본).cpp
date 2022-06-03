b41701b2f0e24f5df1f359085de96c79441864f1
지민 백 <qorwlalsjimin@naver.com>
2022-06-03 10:12:45 +0900

    연산자 오버로딩1

--- 2022CPP/04-01 연산자 오버로딩(기본).cpp
+++ 2022CPP/04-01 연산자 오버로딩(기본).cpp
@@ -2,22 +2,41 @@
 using namespace std;
 
 class Point {
+public:
+	// 생성자
+	Point(int X, int Y);
+	Point();
+
+	// 연산자 오버로딩
+	Point operator+(const Point& rhs);
+	void ShowXY();
+
 private:
 	int mX, mY;
-public:
-	Point(int X, int Y);
-	void ShowXY();
 };
 
 Point::Point(int X, int Y)
 	: mX(X), mY(Y) {};
 
+Point::Point() {};
+
 void Point::ShowXY() {
 	cout << "(" << mX << ", " << mY << ")" << endl;
 }
 
+Point Point::operator+(const Point& rhs) {
+	Point p; //매개변수가 없는 생성자 필요
+	p.mX = mX + rhs.mX;
+	p.mY = mY + rhs.mY;
+	return p;
+	
+}
+
 int main(void) {
 	Point p1(1, 2);
-	p1.ShowXY();
+	Point p2(2, 3);
+	Point sum = p1.operator+(p2); //p1 + p2
+
+	sum.ShowXY(); //3,5
 	return 0;
 }
