d5326c4d913e8cf86a151b76d890d03fb41fea54
지민 백 <qorwlalsjimin@naver.com>
2022-06-07 11:15:39 +0900

    연산자 오버로딩 문제 풀이

---
+++ 2022CPP/04-01 연산자 오버로딩(연습문제).cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+class Integer {
+public:
+	Integer() {};
+	Integer(int v) : value(v) {};
+	void print();
+	Integer operator+(const Integer& rhs);
+
+private:
+	int value;
+};
+
+Integer Integer :: operator+(const Integer& rhs) {
+	Integer i;
+	i.value = value + rhs.value;
+	return i;
+}
+
+void Integer::print() {
+	cout << value << endl;
+}
+
+int main(void) {
+	Integer a = Integer(1);
+	Integer b = Integer(2);
+	Integer c = a+b;
+	c.print(); //3
+
+	return 0;
+}
