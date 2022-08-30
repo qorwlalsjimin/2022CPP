2317afe9ba28da6b67a9f9c11dd1f8733758fa04
지민 백 <qorwlalsjimin@naver.com>
2022-08-30 12:11:50 +0900

    순수가상함수(추상화)

---
+++ 2022CPP/06-03 순수가상함수.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal {
+public:
+	virtual void eat(void) = 0;
+	virtual void roar(void) = 0;
+	virtual void walk(void) = 0;
+
+private:
+	string name;
+	int sex;
+	int age;
+};
+
+class Tiger : public Animal {
+public:
+	void eat(void) override { cout << "호랑이 냠냠" << endl; }
+	void roar(void) override { cout << "어흥" << endl; }
+	void walk(void) override { cout << "호랑이 당당" << endl; }
+};
+
+class Dog : public Animal {
+public:
+	void eat(void) override { cout << "개 쩝쩝" << endl; }
+	void roar(void) override { cout << "멍멍" << endl; }
+	void walk(void) override { cout << "개 사뿐" << endl; }
+};
+
+void main(void) {
+
+	//Animal* animal = new Animal(); //실체가 없는 객체는 생성 불가
+	Animal* animal = new Tiger();
+	delete animal;
+}
