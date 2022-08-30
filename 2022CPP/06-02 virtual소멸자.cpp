879e33c4938cbfa7a2fddc89c7d7aa0b633a3301
지민 백 <qorwlalsjimin@naver.com>
2022-08-30 11:48:06 +0900

    Tiger 소멸자가 호출되지 않음

---
+++ 2022CPP/06-02 virtual소멸자.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal {
+public:
+	Animal() { cout << "Animal 생성" << endl; }
+	~Animal() { cout << "Animal 소멸" << endl; }
+	virtual void roar(void) { cout << "동물짖어" << endl; }
+
+private:
+	string name;
+	int sex;
+	int age;
+};
+
+class Tiger : public Animal {
+public:
+	Tiger() { cout << "Tiger 생성" << endl; }
+	~Tiger() { cout << "Tiger 소멸" << endl; }
+};
+
+void main(void) {
+	Animal* animal = new Tiger();
+	delete animal;
+}
