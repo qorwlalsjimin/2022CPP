4301f96399f342491335c75831accb5deb3e11e2
지민 백 <qorwlalsjimin@naver.com>
2022-10-25 10:51:55 +0900

    범위기반 for

--- 2022CPP/09-02 범위기반.cpp
+++ 2022CPP/09-02 범위기반.cpp
@@ -6,7 +6,6 @@ using namespace std;
 void main(void) {
 	int arr[4] = { 10,20,30,40 };
 
-	for (int i = 0; i < sizeof(arr) / sizeof(int); i++) {
-		cout << arr[i] << " " << endl;
-	}
+	for (int x : arr)
+		cout << x << " ";
 }
