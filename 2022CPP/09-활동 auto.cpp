b111f6f299142da863303d4443311f3609d7fadf
지민 백 <qorwlalsjimin@naver.com>
2022-11-01 10:57:28 +0900

    for문으로 vector 요소에 접근할때 auto 사용

---
+++ 2022CPP/09-활동 auto.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void main(void) {
+
+	//벡터의 요소들을 모두 2배시키는 코드
+	vector<int> v = { 1,2,3,4,5 };
+
+	for (auto& element : v) {
+		element *= 2;
+		cout << element << endl;
+	}
+}
