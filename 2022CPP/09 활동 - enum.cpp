a9a4d677b58a08eb6615221f696ab5a1ba1995d4
지민 백 <qorwlalsjimin@naver.com>
2022-11-01 10:47:42 +0900

    enum 복습

---
+++ 2022CPP/09 활동 - enum.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+enum Fruit {
+	RED,
+	PURPLE,
+	BLUE,
+	YELLOW,
+	COUNT
+};
+
+void main(void) {
+	string fruit_color[Fruit::COUNT];
+
+	fruit_color[Fruit::RED] = "딸기";
+	fruit_color[Fruit::PURPLE] = "포도";
+	fruit_color[Fruit::BLUE] = "블루베리";
+	fruit_color[Fruit::YELLOW] = "바나나";
+
+	cout << "해당색을 가진 과일을 알 수 있어요!" << endl;
+	int input = Fruit::PURPLE;
+
+	switch (input)
+	{
+	case Fruit::RED:
+		cout << "붉은 색은 " << fruit_color[input]; 
+		break;
+	case Fruit::PURPLE:
+		cout << "보라색은 " << fruit_color[input];
+		break;
+	case Fruit::BLUE:
+		cout << "파란 색은 " << fruit_color[input];
+		break;
+	case Fruit::YELLOW:
+		cout << "노란 색은 " << fruit_color[input];
+		break;
+	}
+
+}
